a9d00a8d47a1adf04fa52773b7b93768589b3945
bunleaps <bunleap.sorn@gmail.com>
2024-08-24 15:58:54 +0700

    test Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import Navbar from "./components/templates/Navbar";
 import Footer from "./components/templates/Footer";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 const work_sans = Work_Sans({ subsets: ["latin"] });
 
@@ -14,7 +15,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleAnalytics />
       <body className={work_sans.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KCZVGL5K"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
         <Navbar />
         {children}
         <Footer />
